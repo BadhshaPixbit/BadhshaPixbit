07ed18f35579979f1313e54ac03b0801aa9d2d72
Badhsha <badhsha@outlook.com>
2019-05-06 17:11:26 +0530

    Version 1

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TEST 6</title>
+    <title>TEST 7</title>
 </head>
 <body>
     <?php
-        echo 'TEST 6';
+        echo 'TEST 7';
     ?>
 </body>
 </html>
